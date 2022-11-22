9aeef916f848b2e44387a125048d174e20c475eb
Sumedh Waghmare <sumedhw71@gmail.com>
2022-11-22 23:55:23 +0530

    user add and password hashing

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from . forms import UserForm
+from . models import User
 
 # Create your views here.
 
@@ -9,7 +10,32 @@ def registerUser(request):
 
     if request.method == 'POST':
         print(request.POST)
-        return HttpResponse("ok")
+        form = UserForm(request.POST)
+        if form.is_valid():
+            
+            # create user using form
+            # password = form.changed_data['password']
+            # user = form.save(commit=False)
+            # user.role = User.CUSTOMER
+            # user.set_password(password)
+            # user.save()
+            
+
+            # create user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user  = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+            print('User is created')
+            return redirect('registerUser')
+            
+            
+
     
     if request.method == 'GET':
         form = UserForm()
